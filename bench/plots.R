6738debfe372f7ecf45147264f5626de10672f97
Adam Smelko <adamsmelko1@gmail.com>
2023-12-19 15:49:13 +0100

    update plots

--- bench/plots.R
+++ bench/plots.R
@@ -10,7 +10,7 @@ H=2
 S=1
 point_size=0.8
 line_size=0.5
-linecolors=scale_color_brewer(palette="Set1")
+linecolors=scale_color_brewer(palette="Set2")
 theme = theme_cowplot(font_size=7)
 saturation_multiplier=4000
 
@@ -49,7 +49,25 @@ data_cpu["type"] = "Intel Xeon Gold 6130 CPU"
 data_gpu["type"] = "NVIDIA RTX 3070 Laptop GPU"
 data_v100["type"] = "NVIDIA Tesla V100 GPU"
 data_a100["type"] = "NVIDIA Tesla A100 GPU"
-data = rbind(data_cpu, data_gpu, data_v100, data_a100)
+data = rbind(data_cpu, data_gpu, data_a100)
+
+
+data_gpu_real = read.csv('gpu_out_real.csv', header=T, sep=';')
+data_gpu_real = average(data_gpu_real)
+
+data_a100_real = read.csv('gpu_out_real_a100.csv', header=T, sep=';')
+data_a100_real = average(data_a100_real)
+
+data_cpu_real = read.csv('cpu_out_real.csv', header=T, sep=';')
+names(data_cpu_real)[names(data_cpu_real) == "parsing"] <- "compilation"
+data_cpu_real = subset(data_cpu_real, threads == 64)
+data_cpu_real = subset(data_cpu_real, select = -threads)
+data_cpu_real = average(data_cpu_real)
+
+data_cpu_real["type"] = "Intel Xeon Gold 6130 CPU"
+data_gpu_real["type"] = "NVIDIA RTX 3070 Laptop GPU"
+data_a100_real["type"] = "NVIDIA Tesla A100 GPU"
+data_real = rbind(data_cpu_real, data_gpu_real, data_a100_real)
 
 {
     data_c = data
@@ -75,8 +93,7 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
         xlab("Nodes count (log-scale)")+
         ylab("Wall-time (log-scale)")+
         labs(color="Machine", shape="Machine") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
+        linecolors +
         scale_y_log10(labels = sisec) +
         scale_x_log10(breaks=c(0, 10, 20, 40, 100, 200, 400, 1000)) +
         facet_wrap(~desc) +
@@ -102,20 +119,19 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
         xlab("Nodes count (log-scale)")+
         ylab("Wall-time (log-scale)")+
         labs(color="Machine", shape="Machine") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
+        linecolors +
         scale_y_continuous(labels = scales::percent) +
         scale_x_log10(breaks=c(0, 10, 20, 40, 100, 200, 400, 1000)) +
-        # facet_wrap(~desc) +
-        theme + background_grid() + theme(legend.position="bottom")
+        theme + background_grid() + theme(legend.position="bottom", axis.text.x = element_text(angle = -25, vjust=0.05))
     )
 }
 
+for (m in c("NVIDIA RTX 3070 Laptop GPU", "NVIDIA Tesla A100 GPU"))
 {
     data_c = data
 
     data_c = subset(data_c, max_time == 100)
-    data_c = subset(data_c, type == "NVIDIA RTX 3070 Laptop GPU")
+    data_c = subset(data_c, type == m)
     data_c = subset(data_c, nodes %in% c(200, 400, 600, 800, 1000))
     data_c = subset(data_c, formula_size %in% c(4, 9, 24, 49))
     data_c = subset(data_c, sample_count %in% c(4*10^6, 6*10^6, 8*10^6, 10*10^6))
@@ -127,7 +143,7 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
 
     data_c$sample_count = factor(c("4M", "6M", "8M", "10M"), levels=c("4M", "6M", "8M", "10M"))
 
-    ggsave("nodes-compilation-big.pdf", device='pdf', units="in", scale=S, width=W, height=H,
+    ggsave(paste0("nodes-compilation-big-", m, ".pdf"), device='pdf', units="in", scale=S, width=W, height=H,
         ggplot(data_c, aes(x=nodes,y=ratio, color=sample_count, shape=sample_count)) +
         geom_point(size=point_size) +
         geom_line(linewidth=line_size) +
@@ -135,11 +151,46 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
         ylab("% of total time compiling (log-scale)")+
         labs(color="Sample Count", shape="Sample Count") +
         #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
+        linecolors +
         scale_y_log10(labels = scales::percent) +
         scale_x_continuous(breaks=c(200, 400, 600, 800, 1000)) +
         facet_wrap(~formula_size, ncol=4, labeller=labeller(formula_size=Vectorize(function(x) paste0("Formula size: ", x)))) +
-        theme + background_grid() + theme(legend.position="bottom")
+        theme + background_grid() + theme(legend.position="bottom", axis.text.x = element_text(angle = -25, vjust=0.05))
+    )
+}
+
+{
+    data_c = data_real
+
+    data_c = subset(data_c, name != "metastasis")
+
+    norms = data_c[c("total", "type", "name")]
+
+    data_c["nn"] = 0
+    data_c["diff"] = 0
+    data_c["speedup"] = 0
+
+    for (n in c("cellcycle", "Montagud", "sizek"))
+    {
+        norm = as.numeric(norms[norms$type == "Intel Xeon Gold 6130 CPU" & norms$name == n, ]["total"])
+        data_c <- transform(data_c, nn = ifelse(norms$name == n, norm, nn))
+        data_c <- transform(data_c, diff = ifelse(norms$name == n, total - norm, diff))
+        data_c <- transform(data_c, speedup = ifelse(norms$name == n, norm / total, speedup))
+    }
+
+    ggsave("real.pdf", device='pdf', units="in", scale=S, width=W, height=H,
+        ggplot(data_c, aes(x=type, y=total, fill=type)) +
+        geom_bar(stat="identity", position=position_dodge()) +
+        geom_errorbar(aes(ymin=total, ymax=(total - diff)), width=.1) + 
+        geom_text(aes(label=ifelse(speedup == 1, sitools::f2si(round(total / 10^6, digits=2), 's'), paste0('-',round(speedup, digits=0),'x')), y=total), size=2, vjust=0.5*3) + 
+        geom_hline(aes(yintercept=nn), linewidth=.3) +
+        xlab("")+
+        ylab("Wall-time (sqrt-scale)")+
+        labs(color="Machine", shape="Machine", fill="Machine") +
+        scale_fill_brewer(palette="Set2") +
+        scale_y_continuous(trans=scales::trans_new('my_sqrt', function (x) x ^ (1/4), function (x) x^4), labels = sisec, breaks=c(100 * 10^6, 10 * 10^6, 100 * 10^3, 500 * 10^3)) +
+        facet_wrap(~name, ncol=4, scales="free_y") +
+        theme + background_grid() + theme(legend.position="bottom", axis.title.x=element_blank(), axis.text.x=element_blank(), axis.ticks.x=element_blank())
     )
 }
 
@@ -147,7 +198,7 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
     data_c = data
 
     data_c = subset(data_c, max_time == 100)
-    data_c = subset(data_c, type == "NVIDIA RTX 3070 Laptop GPU")
+    data_c = subset(data_c, type == "NVIDIA Tesla A100 GPU")
     data_c = subset(data_c, nodes %in% c(200, 400, 600, 800, 1000))
     data_c = subset(data_c, formula_size %in% c(4, 9, 24, 49))
     data_c = subset(data_c, sample_count %in% c(4*10^6, 6*10^6, 8*10^6, 10*10^6))
@@ -163,8 +214,7 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
         xlab("Nodes count (log-scale)")+
         ylab("Normalized time per op (log-scale)")+
         labs(color="Sample Count", shape="Sample Count") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
+        linecolors +
         scale_y_log10(labels = sisec) +
         scale_x_continuous(breaks=c(200, 400, 600, 800, 1000)) +
         facet_wrap(~formula_size, ncol=4, labeller=labeller(formula_size=Vectorize(function(x) paste0("Formula size: ", x)))) +
@@ -172,83 +222,80 @@ data = rbind(data_cpu, data_gpu, data_v100, data_a100)
     )
 }
 
-{
-    data_c = data
+# {
+#     data_c = data
 
-    data_c = subset(data_c, max_time == 100)
-    data_c = subset(data_c, nodes == 100)
-    data_c = subset(data_c, formula_size == 4)
-    # data_c = subset(data_c, sample_count == 1000000)
+#     data_c = subset(data_c, max_time == 100)
+#     data_c = subset(data_c, nodes == 100)
+#     data_c = subset(data_c, formula_size == 4)
+#     # data_c = subset(data_c, sample_count == 1000000)
 
-    data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
+#     data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
 
-    # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
+#     # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
 
-    ggsave("samples-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
-        ggplot(data_c, aes(x=sample_count,y=simulation/sample_count, color=factor(type), shape=factor(type))) +
-        geom_point(size=point_size) +
-        geom_line(linewidth=line_size) +
-        xlab("Samples count (log-scale)")+
-        ylab("Normalized time per sample (log-scale)")+
-        labs(color="Sample Count", shape="Sample Count") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
-        scale_y_log10(labels = sisec) +
-        scale_x_log10(labels=function(x) paste0(x/10^6,"M"), breaks=c(1*10^6,2*10^6,4*10^6,10*10^6)) +
-        theme + background_grid() + theme(legend.position="bottom")
-    )
-}
+#     ggsave("samples-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
+#         ggplot(data_c, aes(x=sample_count,y=simulation/sample_count, color=factor(type), shape=factor(type))) +
+#         geom_point(size=point_size) +
+#         geom_line(linewidth=line_size) +
+#         xlab("Samples count (log-scale)")+
+#         ylab("Normalized time per sample (log-scale)")+
+#         labs(color="Sample Count", shape="Sample Count") +
+#         linecolors +
+#         scale_y_log10(labels = sisec) +
+#         scale_x_log10(labels=function(x) paste0(x/10^6,"M"), breaks=c(1*10^6,2*10^6,4*10^6,10*10^6)) +
+#         theme + background_grid() + theme(legend.position="bottom")
+#     )
+# }
 
-{
-    data_c = data
+# {
+#     data_c = data
 
-    # data_c = subset(data_c, max_time == 100)
-    data_c = subset(data_c, nodes == 100)
-    data_c = subset(data_c, formula_size == 4)
-    data_c = subset(data_c, sample_count == 1000000)
+#     # data_c = subset(data_c, max_time == 100)
+#     data_c = subset(data_c, nodes == 100)
+#     data_c = subset(data_c, formula_size == 4)
+#     data_c = subset(data_c, sample_count == 1000000)
 
-    data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
+#     data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
 
-    # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
+#     # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
 
-    ggsave("max-time-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
-        ggplot(data_c, aes(x=max_time,y=simulation/max_time, color=factor(type), shape=factor(type))) +
-        geom_point(size=point_size) +
-        geom_line(linewidth=line_size) +
-        xlab("Samples count (log-scale)")+
-        ylab("Normalized time per time step (log-scale)")+
-        labs(color="Sample Count", shape="Sample Count") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
-        scale_y_log10(labels = sisec) +
-        scale_x_log10(breaks=c(100,200,400,800,1000)) +
-        theme + background_grid() + theme(legend.position="bottom")
-    )
-}
+#     ggsave("max-time-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
+#         ggplot(data_c, aes(x=max_time,y=simulation/max_time, color=factor(type), shape=factor(type))) +
+#         geom_point(size=point_size) +
+#         geom_line(linewidth=line_size) +
+#         xlab("Samples count (log-scale)")+
+#         ylab("Normalized time per time step (log-scale)")+
+#         labs(color="Sample Count", shape="Sample Count") +
+#         linecolors +
+#         scale_y_log10(labels = sisec) +
+#         scale_x_log10(breaks=c(100,200,400,800,1000)) +
+#         theme + background_grid() + theme(legend.position="bottom")
+#     )
+# }
 
-{
-    data_c = data
+# {
+#     data_c = data
 
-    data_c = subset(data_c, max_time == 100)
-    data_c = subset(data_c, nodes == 100)
-    # data_c = subset(data_c, formula_size == 4)
-    data_c = subset(data_c, sample_count == 1000000)
+#     data_c = subset(data_c, max_time == 100)
+#     data_c = subset(data_c, nodes == 100)
+#     # data_c = subset(data_c, formula_size == 4)
+#     data_c = subset(data_c, sample_count == 1000000)
 
-    data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
+#     data_c["formula_size"] =  2 * (data_c["formula_size"] + 1)
 
-    # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
+#     # data_c$sample_count2 = factor(c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"), levels=c("1M", "2M", "3M", "4M", "5M", "6M", "7M", "8M", "9M", "10M"))
 
-    ggsave("formulas-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
-        ggplot(data_c, aes(x=formula_size,y=simulation/formula_size, color=factor(type), shape=factor(type))) +
-        geom_point(size=point_size) +
-        geom_line(linewidth=line_size) +
-        xlab("Samples count (log-scale)")+
-        ylab("Normalized time per time step (log-scale)")+
-        labs(color="Sample Count", shape="Sample Count") +
-        #scale_color_manual(values=RColorBrewer::brewer.pal(9,'YlGnBu')[2:9]) +
-        scale_color_brewer(palette="Paired") +
-        scale_y_log10(labels = sisec) +
-        scale_x_log10(breaks=c(10,20,40,80,100)) +
-        theme + background_grid() + theme(legend.position="bottom")
-    )
-}
+#     ggsave("formulas-normalized.pdf", device='pdf', units="in", scale=S, width=W, height=H,
+#         ggplot(data_c, aes(x=formula_size,y=simulation/formula_size, color=factor(type), shape=factor(type))) +
+#         geom_point(size=point_size) +
+#         geom_line(linewidth=line_size) +
+#         xlab("Samples count (log-scale)")+
+#         ylab("Normalized time per time step (log-scale)")+
+#         labs(color="Sample Count", shape="Sample Count") +
+#         linecolors +
+#         scale_y_log10(labels = sisec) +
+#         scale_x_log10(breaks=c(10,20,40,80,100)) +
+#         theme + background_grid() + theme(legend.position="bottom")
+#     )
+# }
